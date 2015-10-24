3213348d35623585a2870ca8342f97787b8a95c2
vishaag <vishaag.s@gmail.com>
2015-10-25 01:33:32 +0530

    second commit

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
   $('.collapsible').collapsible();
+  $('.scrollspy').scrollSpy();
 
   $('.dropdown-button').dropdown({
       inDuration: 300,
@@ -8,7 +9,7 @@ $(document).ready(function(){
       constrain_width: true, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
       gutter:0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
+      belowOrigin: false, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
     }
   );
